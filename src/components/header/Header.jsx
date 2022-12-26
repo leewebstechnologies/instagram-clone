eb8ae69e3efc966e7e232c72660ec5e68f14e6db
leewebstechnologies <aliuajibade317@gmail.com>
2022-12-25 18:14:24 -0800

    header folder completed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,6 +6,7 @@ const Header = () => {
       <div className="header">
         <div className="headerWrapper">
           <img
+          className="logo"
             src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
             alt="Instagram original logo"
           />
