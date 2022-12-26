ab652b424e1044749c6209b2358b9e60be998b6d
leewebstechnologies <aliuajibade317@gmail.com>
2022-12-25 17:53:37 -0800

    header component being styled

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,23 @@
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="headerContainer">
+      <div className="header">
+        <div className="headerWrapper">
+          <img
+            src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+            alt="Instagram original logo"
+          />
+          <button className="logout__btn">Logout</button>
+          <div className="btn">
+            <button className="login__btn">Login</button>
+            <button className="signup__btn">Sign Up</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
